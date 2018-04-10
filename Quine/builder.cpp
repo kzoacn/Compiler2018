ee32e082d9aefd925374f2cd866925a61fda6114
kzoacn <kzoacn@outlook.com>
2018-04-10 19:14:44 +0800

    cpp quine

---
+++ Quine/builder.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void head(){
+    cout<<"#include<iostream>"<<endl;
+    cout<<"using namespace std;"<<endl;
+    cout<<"void println(string s){"<<endl;
+    cout<<"    cout<<s<<endl;"<<endl;
+    cout<<"}"<<endl;
+}
+vector<string>s,c;
+void modifySlast3(){
+    int size=s.size()+5;
+    s.push_back("println(s[0]);");
+    s.push_back("for(i=0;i<"+to_string(c.size())+";i++)println(c2(i)+a2q+c[i]+a2q+co);");
+    s.push_back("for(i=0;i<"+to_string(size)+";i++)println(s2(i)+a2q+s[i]+a2q+co);");
+    s.push_back("for(i=1;i<"+to_string(size)+";i++)println(s[i]);");
+    s.push_back("}");
+}
+
+map<char,int>C;
+
+string sym(char c){
+    if(c=='\\')return "a2b";
+    if(c=='\"')return "a2q";
+    return "c["+to_string(C[c])+"]";
+}
+
+string trans(string s){
+    string ans;
+    ans=sym(s[0]);
+    for(int i=1;i<s.length();i++)
+        ans=ans+("+")+sym(s[i]);
+    return ans;
+}
+
+vector<string>vec;
+
+int main(){
+    vec.push_back("string di(int x){");
+    vec.push_back("if(x==0)return \"0\";");
+    vec.push_back("if(x==1)return \"1\";");
+    vec.push_back("if(x==2)return \"2\";");
+    vec.push_back("if(x==3)return \"3\";");
+    vec.push_back("if(x==4)return \"4\";");
+    vec.push_back("if(x==5)return \"5\";");
+    vec.push_back("if(x==6)return \"6\";");
+    vec.push_back("if(x==7)return \"7\";");
+    vec.push_back("if(x==8)return \"8\";");
+    vec.push_back("if(x==9)return \"9\";");
+    vec.push_back("}");
+    
+    vec.push_back("string s2(int x){");
+    vec.push_back("if(x<=9)return \"s[\"+di(x)+\"]=\";");
+    vec.push_back("return \"s[\"+di(x/10)+di(x%10)+\"]=\";");
+    vec.push_back("}");
+    
+    vec.push_back("string c2(int x){");
+    vec.push_back("if(x<=9)return \"c[\"+di(x)+\"]=\";");
+    vec.push_back("return \"c[\"+di(x/10)+di(x%10)+\"]=\";");
+    vec.push_back("}");
+    
+    vec.push_back("string co=\";\";");
+    vec.push_back("string a2q=\"\\\"\";");
+    vec.push_back("string a2b=\"\\\\\";");
+    vec.push_back("string s[256];");
+    vec.push_back("string c[256];");
+
+    head();
+    int cur=0;
+    for(int i=32;i<=126;i++){
+        if(i=='\\')continue;
+        if(i=='\"')continue;
+        C[(char)i]=cur++;
+        c.push_back(string(1,(char)i));
+    }
+    for(auto str:vec)
+        cout<<str<<endl;
+    s.push_back("int main(){int i=0;");
+    cout<<s[0]<<endl;
+    for(int i=0;i<c.size();i++)
+        printf("c[%d]=\"%s\";\n",i,c[i].c_str());
+    
+    for(auto str:vec)
+        s.push_back("println("+trans(str)+");");
+    
+    modifySlast3();
+    for(int i=0;i<s.size();i++)
+        printf("s[%d]=\"%s\";\n",i,s[i].c_str());
+    for(int i=1;i<s.size();i++)
+        printf("%s\n",s[i].c_str());
+    return 0;
+}
--- Quine/q.cpp
+++ Quine/q.cpp
@@ -16,29 +16,35 @@ if(x==7)return "7";
 if(x==8)return "8";
 if(x==9)return "9";
 }
-string n2(int x){
+string s2(int x){
 if(x<=9)return "s["+di(x)+"]=";
 return "s["+di(x/10)+di(x%10)+"]=";
+}
+string c2(int x){
+if(x<=9)return "c["+di(x)+"]=";
+return "c["+di(x/10)+di(x%10)+"]=";
 }
 string co=";";
 string a2q="\"";
 string a2b="\\";
-string s[20];int main(){int q=34;int b=92;int n=10;int i=0;
-s[0]="string s[20];int main(){int q=34;int b=92;int n=10;int i=0;";
-s[1]="println(s[6]+q+co+q+co);";
-s[2]="println(s[7]+a2q+a2b+a2q+a2q+co);";
-s[3]="println(s[8]+a2q+a2b+a2b+a2q+co);";
+string s[20];string c[20];int main(){int i=0;
+c[0]="string co=";
+c[1]="string a2q=";
+c[2]="string a2b=";
+s[0]="string s[20];string c[20];int main(){int i=0;";
+s[1]="println(c[0]+a2q+co+a2q+co);";
+s[2]="println(c[1]+a2q+a2b+a2q+a2q+co);";
+s[3]="println(c[2]+a2q+a2b+a2b+a2q+co);";
 s[4]="println(s[0]);";
-s[5]="for(i=0;i<=10;i++)println(n2(i)+a2q+s[i]+a2q+co);";
-s[6]="for(i=1;i<=7;i++)println(s[i]);";
-s[7]="}";
-s[8]="string co=";
-s[9]="string a2q=";
-s[10]="string a2b=";
-println(s[8]+a2q+co+a2q+co);
-println(s[9]+a2q+a2b+a2q+a2q+co);
-println(s[10]+a2q+a2b+a2b+a2q+co);
+s[5]="for(i=0;i<=8;i++)println(n2(i)+a2q+s[i]+a2q+co);";
+s[6]="for(i=0;i<=2;i++)println(c2(i)+a2q+c[i]+a2q+co);";
+s[7]="for(i=1;i<=8;i++)println(s[i]);";
+s[8]="}";
+println(c[0]+a2q+co+a2q+co);
+println(c[1]+a2q+a2b+a2q+a2q+co);
+println(c[2]+a2q+a2b+a2b+a2q+co);
 println(s[0]);
-for(i=0;i<=10;i++)println(n2(i)+a2q+s[i]+a2q+co);
-for(i=1;i<=7;i++)println(s[i]);
+for(i=0;i<=2;i++)println(c2(i)+a2q+c[i]+a2q+co);
+for(i=0;i<=8;i++)println(s2(i)+a2q+s[i]+a2q+co);
+for(i=1;i<=8;i++)println(s[i]);
 }
