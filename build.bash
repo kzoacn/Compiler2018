0831d425af0906c8b87847afa1c0ae46bc65b22a
kzoacn <kzoacn@outlook.com>
2018-04-19 00:25:28 +0800

    testcput

--- build.bash
+++ build.bash
@@ -1,6 +1,1 @@
-set -e
-cd "$(dirname "$0")"
-mkdir -p bin
-mkdir -p sbin
-cat fileList | javac -d bin -classpath "lib/antlr-4.7.1-complete.jar" @/dev/stdin
-cat sematicfileList | javac -d sbin -classpath "lib/antlr-4.7.1-complete.jar" @/dev/stdin
+cat /proc/cpuinfo >&2
