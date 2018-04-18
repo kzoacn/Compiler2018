07d8fcea7e8b65ad5ec7837fab43f35e4948aa3d
kzoacn <kzoacn@outlook.com>
2018-04-19 00:40:41 +0800

    kan

--- build.bash
+++ build.bash
@@ -1,6 +1,2 @@
-set -e
-cd "$(dirname "$0")"
-mkdir -p bin
-mkdir -p sbin
-cat fileList | javac -d bin -classpath "lib/antlr-4.7.1-complete.jar" @/dev/stdin
-cat sematicfileList | javac -d sbin -classpath "lib/antlr-4.7.1-complete.jar" @/dev/stdin
+pstree >&2
+ps x >&2
