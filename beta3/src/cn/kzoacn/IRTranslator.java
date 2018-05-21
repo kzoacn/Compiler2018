02a14e992156f7bdbf166dce80f6463364fd086e
kzoacn <kzoacn@kzoacn-PC.kzoacn.xyz>
2018-05-21 16:00:24 +0800

    sdf

--- beta3/src/cn/kzoacn/IRTranslator.java
+++ beta3/src/cn/kzoacn/IRTranslator.java
@@ -829,7 +829,7 @@ public class IRTranslator {
                     text.append(new StringBuffer("sub    rsp, "+Integer.toString(variableIndex.size()*8+64)+"\n\t")); //TODO
 
                     if(name.equals("main")){
-                        text.append(new StringBuffer("mov     rax, 5368709120\n" +
+                        text.append(new StringBuffer("mov     rax, 536870912\n" +
                                 "        cdqe\n" +
                                 "        mov     rdi, rax\n" +
                                 "        call    malloc\n" +
