57ba5d3015a85a672373c6b6d0379ca9f7840e95
kzoacn <kzoacn@kzoacn-PC.kzoacn.xyz>
2018-05-25 19:25:37 +0800

    OneMoreTimeOneMoreChance

--- build.bash
+++ build.bash
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 mkdir -p bin
 mkdir -p sbin
 g++ inputbuilder.cpp -o inputbuilder
-g++ movclear.cpp -o movclear
+g++ movclear.cpp -o movclear -std=c++11
 chmod +x c2nasm.sh
 chmod +x ./objc/src/build.sh
 g++ -o objconv ./objc/src/*.cpp -std=c++03
