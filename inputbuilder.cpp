a5badc94aae3dc5e7f28c7f56d861a3648d5916e
kzoacn <kzoacn@kzoacn-PC.kzoacn.xyz>
2018-05-30 21:51:01 +0800

    Yes,My Lord

--- inputbuilder.cpp
+++ inputbuilder.cpp
@@ -4,11 +4,21 @@ int main(){
 	string s;
 	cout<<"#include<stdio.h>"<<endl;
 	cout<<"int main(){"<<endl;
+	
+	string ans;
+	
 	while(1){
 		int x=cin.get();
 		if(x==EOF)break;
-		cout<<"putchar("<<x<<");"<<endl;
+		if(x==10)ans+="\\",ans+="n";
+		else if(x=='\\')ans+="\\\\";
+		else if(x=='"')ans+="\\\"";
+		else ans+=string(1,char(x));
 	}
+	
+	
+	
+		cout<<"printf(\""<<ans<<"\");"<<endl;
 	cout<<"}"<<endl;
 	
 	return 0;
